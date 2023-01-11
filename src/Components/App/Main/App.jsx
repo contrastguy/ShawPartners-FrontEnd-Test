7909790a9a00b3824cc5c7ad71bd2dd7e4ddbca6
Matheus Rodrigues <62529344+contrastguy@users.noreply.github.com>
2023-01-11 20:09:24 -0300

    Removing LocalStorage

--- src/Components/App/Main/App.jsx
+++ src/Components/App/Main/App.jsx
@@ -14,32 +14,40 @@ const UserList = () => {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [number, setNumber] = useState(1);
+  const [error, setError] = useState(null);
+  const [number, setNumber] = useState(10);
+  const [offset, setOffset] = useState(0);
   const navigator = useNavigate();
+
   const moreUsers = () => {
     setPage(page + 1);
-    localStorage.setItem("NUMBER", (number + 5));
+    setNumber((p) => p + 10);
+    setOffset(o => o >= 40 ? 40 : o+10)
+    // Quando clica no botão, starta o useEffect e o 15 vira o valor inicial ,porém é somado ou diminuido
   };
   const lessUsers = () => {
     setPage(page - 1);
-    localStorage.setItem("NUMBER", (number - 5));
+    setNumber((p) => p - 10);
+    setOffset(o => o <= 0 ? 0 : o-10)
+    //page começa com 15, toda vez que atualiza ou clicka em um botao
   };
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      setLoading(true);
-      //A url do back end funciona aqui
-      const response = await api.get("/users");
-      setUsers(response.data);
-      setLoading(false);
-    };
-    
+    try {
+      const fetchUsers = async () => {
+        setLoading(true);
+        //A url do back end funciona aqui
+        const response = await api.get("/users");
+        setUsers(response.data);
+        setLoading(false);
+      };
 
-    fetchUsers();
-    setNumber(15);
-    localStorage.setItem("NUMBER", number);
-  }, [number]);
-
+      fetchUsers()
+    } catch (error) {
+      setError("Something failed!");
+    }
+  }, []);
+  const usersNumber = +localStorage.getItem('NUMBER') 
   return (
     <>
       <h1 id="title">Users List</h1>
@@ -49,7 +57,7 @@ const UserList = () => {
         ) : (
           <ListGroup class="list">
             {users.map((user, index) =>
-              index <= (localStorage.getItem("NUMBER")) ? (
+             index+1 >= offset && index+1 <= number? (
                 <>
                   <div class="list-item">
                     <ListGroup.Item
@@ -59,7 +67,7 @@ const UserList = () => {
                         navigator(`/users/${localStorage.getItem("LOGIN")}`);
                       }}
                     >
-                      ID: {user.id} / Login: {user.login}
+                     Number {index+1} ID: {user.id} / Login: {user.login}
                     </ListGroup.Item>
                   </div>
                 </>
@@ -70,13 +78,23 @@ const UserList = () => {
         <div class="pagination">
           <Pagination.Item
             //Quando eu clico no botão , volta a lista com os mesmos usuários
-            onClick={() => {lessUsers()}}
+            // O evento chama uma função que foi criada
+            onClick={() => {
+              lessUsers();
+            }}
             disabled={page === 1}
           >
             Previous
           </Pagination.Item>
-          <Pagination.Item onClick={() => {moreUsers()}}>Next</Pagination.Item>
+          <Pagination.Item
+            onClick={() => {
+              moreUsers();
+            }}
+          >
+            Next
+          </Pagination.Item>
         </div>
+
       </div>
     </>
   );
